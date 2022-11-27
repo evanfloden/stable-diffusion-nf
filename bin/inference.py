bb70eee3340729fb97a3fcf2a955d364e3b51840
Evan Floden <evanfloden@gmail.com>
2022-11-27 04:13:25 +0000

    Added .gitignore

--- bin/inference.py
+++ bin/inference.py
@@ -2,3 +2,12 @@
 import torch
 from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 
+prompt = "horse watching cricket smoking a vape"
+
+model_id = "stabilityai/stable-diffusion-2"
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler,torch_dtype=torch.float16) # revision="fp16"
+
+image = pipe(prompt, height=768, width=768).images[0]
+buf = io.BytesIO()
+image.save(buf, format="PNG")
