ac97c17aa1f8f3c0dd0b9c5ec80760cf1f97822f
Evan Floden <evanfloden@gmail.com>
2022-11-27 05:55:33 +0000

    First run!

--- bin/inference.py
+++ bin/inference.py
@@ -3,11 +3,10 @@ import torch
 from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 
 prompt = "horse watching cricket smoking a vape"
-
 model_id = "stabilityai/stable-diffusion-2"
-scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler,torch_dtype=torch.float16) # revision="fp16"
 
+#scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+
+pipe = StableDiffusionPipeline.from_pretrained(model_id) #, scheduler=scheduler,revision="fp16", torch_dtype=torch.float16) 
 image = pipe(prompt, height=768, width=768).images[0]
-buf = io.BytesIO()
-image.save(buf, format="PNG")
+image.save("astronaut_rides_horse.png")
