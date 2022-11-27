9cf3829037a24cc90b533458848076ff7375ebf6
Evan Floden <evanfloden@gmail.com>
2022-11-27 03:46:46 +0000

    Updated to v2

--- bin/inference.py
+++ bin/inference.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
 import torch
-from transformers import CLIPTextModel, CLIPTokenizer
-from diffusers import AutoencoderKL, UNet2DConditionModel, LMSDiscreteScheduler
-from tqdm.auto import tqdm
-from torch import autocast
-from PIL import Image
-from matplotlib import pyplot as plt
-import numpy
-from torchvision import transforms as tfms
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+
